cbf3fb05055f8141abde029e8ba4cf8233d3deb7
bizbaeja <bjgbjg112@gmail.com>
2024-03-22 14:24:32 +0900

    Merge branch 'main' of https://github.com/aayyybbb/gitExamRepository.git into main

--- gitWeb/src/main/java/gitWeb/Hello.java
+++ gitWeb/src/main/java/gitWeb/Hello.java
@@ -2,8 +2,14 @@ package gitWeb;
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("경덕");
-		System.out.println("자현");
+	    System.out.println("경덕");
+		
+		System.out.println("경덕2");
+
+		
+		System.out.println("경덕2");
+
 		System.out.println("연규");
+
 	}
 }
